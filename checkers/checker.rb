e9ed601fe6818c6e60ec5b78c90b1350b5ef7a25
xier <xier.kokis@gmail.com>
2016-03-09 16:59:05 +0700

    refactor

--- checkers/checker.rb
+++ checkers/checker.rb
@@ -3,33 +3,16 @@ module Checkers
 
     def is_more_than_threshold(threshold_value, data_points)
       points = reformat_data(data_points)
-      status = false
-
-      points.each do |data_point|
-        if data_point >= threshold_value
-          status = true
-          break
-        end
-      end
-      status
+      points.all? { |p| p >= threshold_value }
     end
 
     def is_less_than_threshold(threshold_value, data_points)
       points = reformat_data(data_points)
-      status = false
-
-      points.each do |data_point|
-        if data_point <= threshold_value
-          status = true
-          break
-        end
-      end
-      status
+      points.all? { |p| p <= threshold_value }
     end
 
     def is_dead(data_points)
-      points = reformat_data(data_points)
-      points.inject(0){|sum,x| sum + x } == 0
+      points.all?(&:blank?)
     end
 
     def reformat_data(data_points)
